97158106367e9eed1786ac0c575179046f82975e
ricardocho1198 <ricardocho1198@gmail.com>
2015-07-13 00:58:06 -0500

    Add modulo noticias

---
+++ Bow.Core/Administracion/Repositorios/INoticiasRepositorio.cs
@@ -0,0 +1,14 @@
+﻿using Abp.Domain.Repositories;
+using Bow.Administracion.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bow.Administracion.Repositorios
+{
+    public interface INoticiasRepositorio : IRepository<Noticias>
+    {
+    }
+}
