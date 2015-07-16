2e17bf0baf10535f28b94514d7a7b5a38e411c1c
ricardocho1198 <ricardocho1198@gmail.com>
2015-07-15 21:02:26 -0500

    diagnostico -editar

---
+++ Bow.Web/App/Main/views/administracion/diagnostico/partials/modalEditarDiagnostico.js
@@ -0,0 +1,28 @@
+﻿(function () {
+    angular.module('app').controller('modalEditarDiagnosticoController', ['$scope', '$modalInstance', 'diagnosticoEditar', 'abp.services.app.administracion',
+        function ($scope, $modalInstance, diagnosticoEditar, administracionService) {
+
+            $scope.diagnostico = {
+                nombre: '',
+                observaciones: ''
+            };
+
+            administracionService.getGetItemByDiagnosticoVialNoticias({ id: diagnosticoEditar })
+                .success(function (data) {
+                    $scope.diagnostico = data;
+                });
+
+            $scope.okModal = function () {
+                administracionService.updateItemDiagnosticoVial($scope.diagnostico)
+                    .success(function () {
+                        $modalInstance.close($scope.diagnostico.nombre);
+                    }).error(function (error) {
+                        $scope.mensajeError = error.message;
+                    });
+            }
+
+            $scope.cancelModal = function () {
+                $modalInstance.dismiss('cancel');
+            }
+        }]);
+})();
